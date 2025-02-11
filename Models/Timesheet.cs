340d494aa4a1d0b1d614778321e5b158cd3d4451
kasim <kasim@LAPTOP-DKC9CIN0>
2025-02-11 18:25:35 +0000

    Add project files.

---
+++ Models/Timesheet.cs
@@ -0,0 +1,13 @@
+﻿namespace TimesheetApp.Models
+{
+    public class Timesheet
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Project { get; set; }
+
+        public string DescriptionOfTasks { get; set; }
+
+        public int HoursWorked { get; set; }
+    }
+}
