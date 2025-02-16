33ca3c0147dad777d59ae10995e70ee5bc2f99cc
kasim <kasim>
2025-02-16 13:15:48 +0000

    Unit tests added, added js to enable csv download

---
+++ Tests/TimesheetTests.cs
@@ -0,0 +1,60 @@
+﻿using Moq;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using TimesheetApp.BusinessLogic;
+using TimesheetApp.Data.Repositories.Interfaces;
+using TimesheetApp.Models.EntityModels;
+
+namespace TimesheetApp.Tests
+{
+    [TestFixture]
+    public class TimesheetTests
+    {
+
+        private Mock<ITimesheetRepository> _mockRepo;
+        private TimesheetManager _timesheetManager;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockRepo = new Mock<ITimesheetRepository>();
+            _timesheetManager = new TimesheetManager(_mockRepo.Object);
+        }
+
+        [Test]
+        public void CreateTimesheet_Success()
+        {
+            var timesheet = new Timesheet
+            {
+                Name = "Tester",
+                Date = DateTime.Now,
+                Project = "TestProject",
+                HoursWorked = 8,
+                DescriptionOfTasks = "Testing"
+            };
+
+            _timesheetManager.CreateTimesheet(timesheet);
+
+            _mockRepo.Verify(r => r.CreateTimesheet(It.IsAny<Timesheet>()), Times.Once);
+        }
+
+        [Test]
+        public void GetTimesheets_ReturnsCorrectTimesheets()
+        {
+
+            var timesheets = new List<Timesheet>
+                {
+                    new Timesheet { Id = 1, Name = "Tester1", Date = DateTime.Now, Project = "TestProject1", HoursWorked = 8, DescriptionOfTasks = "Testing1" },
+                    new Timesheet { Id = 2, Name = "Tester2", Date = DateTime.Now, Project = "TestProject2", HoursWorked = 7, DescriptionOfTasks = "Testing2" }
+                };
+
+            _mockRepo.Setup(r => r.GetTimesheets()).Returns(timesheets);
+
+            var result = _timesheetManager.GetTimesheets();
+
+            ClassicAssert.AreEqual(2, result.Count);
+            ClassicAssert.AreEqual("Tester1", result[0].Name);
+            ClassicAssert.AreEqual("Tester2", result[1].Name);
+        }
+    }
+}
