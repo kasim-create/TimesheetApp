1cea2fbbd630a72c3b0c86e53277063c2cd9bc3a
kasim <kasim>
2025-02-16 12:20:06 +0000

    Added TimesheetController , generated and customised views

---
+++ Controllers/TimesheetController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TimesheetApp.BusinessLogic;
+using TimesheetApp.Data;
+using TimesheetApp.Models;
+using TimesheetApp.Models.EntityModels;
+
+namespace TimesheetApp.Controllers
+{
+    public class TimesheetController : Controller
+    {
+        private readonly TimesheetManager _timesheetManager;
+
+       public TimesheetController(TimesheetManager timesheetManager)
+        {
+            _timesheetManager = timesheetManager;
+        }
+
+        [HttpGet("Timesheets")]
+        public IActionResult Timesheets()
+        {
+            var timesheets = _timesheetManager.GetTimesheets()
+                .Select(t => new TimesheetViewModel
+                {
+                    Id = t.Id,
+                    Name = t.Name,
+                    Date = t.Date,
+                    Project = t.Project,
+                    DescriptionOfTasks = t.DescriptionOfTasks,
+                    HoursWorked = t.HoursWorked
+                })
+                .ToList();
+
+            if (timesheets != null)
+            {
+                return View(timesheets);
+            }
+            
+            return View();
+        }
+
+        [HttpGet("CreateTimesheet")]
+        public IActionResult CreateTimesheet()
+        {
+            return View();
+        }
+
+        [HttpPost("CreateTimesheet")]
+        public IActionResult CreateTimesheet(TimesheetViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var timesheet = new Timesheet
+                {
+                    Name = model.Name,
+                    Date = model.Date,
+                    Project = model.Project,
+                    DescriptionOfTasks = model.DescriptionOfTasks,
+                    HoursWorked = model.HoursWorked
+                };
+
+                _timesheetManager.CreateTimesheet(timesheet);
+                return RedirectToAction("Timesheets");
+            }
+            return View(model);
+        }
+    }
+}
